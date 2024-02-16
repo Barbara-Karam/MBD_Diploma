803743501fced7c16d8e0657214e72feaf9ae857
Barbara-Karam <104846547+Barbara-Karam@users.noreply.github.com>
2024-02-16 19:16:11 +0200

    better organization

--- MATLAB-LAB3/Basic_Plotting.m
+++ MATLAB-LAB3/Basic_Plotting.m
@@ -1,7 +1,7 @@
 x= linspace(-2*pi,2*pi,100);
 y1=sin(x);
 y2=cos(x);
-plot(x,y1,'r',x,y2,'b');
+plot(x,y1,'Color','red','line','dashed','marker','square');
 xlabel('X-Axix');
 xlabel('Y-Axix');
 legend('sin x','cos x')
