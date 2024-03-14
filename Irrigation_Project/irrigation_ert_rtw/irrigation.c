35dcf6499a084e2dc0111dc1e4c35a28564f06b6
Barbara-Karam <104846547+Barbara-Karam@users.noreply.github.com>
2024-03-14 19:11:18 +0200

    Completed Irrigation Project

---
+++ Irrigation_Project/irrigation_ert_rtw/irrigation.c
@@ -0,0 +1,106 @@
+/*
+ * File: irrigation.c
+ *
+ * Code generated for Simulink model 'irrigation'.
+ *
+ * Model version                  : 1.1
+ * Simulink Coder version         : 9.8 (R2022b) 13-May-2022
+ * C/C++ source code generated on : Thu Mar 14 19:05:34 2024
+ *
+ * Target selection: ert.tlc
+ * Embedded hardware selection: Atmel->AVR
+ * Code generation objectives:
+ *    1. Execution efficiency
+ *    2. RAM efficiency
+ * Validation result: Not run
+ */
+
+#include "irrigation.h"
+
+/* Private macros used by the generated code to access rtModel */
+#ifndef rtmIsMajorTimeStep
+#define rtmIsMajorTimeStep(rtm)        (((rtm)->Timing.simTimeStep) == MAJOR_TIME_STEP)
+#endif
+
+#ifndef rtmIsMinorTimeStep
+#define rtmIsMinorTimeStep(rtm)        (((rtm)->Timing.simTimeStep) == MINOR_TIME_STEP)
+#endif
+
+#ifndef rtmSetTPtr
+#define rtmSetTPtr(rtm, val)           ((rtm)->Timing.t = (val))
+#endif
+
+/* Block signals and states (default storage) */
+DW rtDW;
+
+/* External outputs (root outports fed by signals with default storage) */
+ExtY rtY;
+
+/* Real-time model */
+static RT_MODEL rtM_;
+RT_MODEL *const rtM = &rtM_;
+
+/* Model step function */
+void irrigation_step(void)
+{
+  /* Outputs for IfAction SubSystem: '<Root>/If Action Subsystem1' incorporates:
+   *  ActionPort: '<S4>/Action Port'
+   */
+  /* If: '<Root>/If' incorporates:
+   *  Outport: '<Root>/Out1'
+   *  SignalConversion generated from: '<S4>/RED LED (Error)'
+   */
+  rtY.Out1 = 0.0;
+
+  /* End of Outputs for SubSystem: '<Root>/If Action Subsystem1' */
+
+  /* Outport: '<Root>/water pump' */
+  rtY.waterpump = rtDW.Compare_k;
+
+  /* Outport: '<Root>/Out2' */
+  rtY.Out2 = rtDW.Compare;
+
+  /* Update absolute time for base rate */
+  /* The "clockTick0" counts the number of times the code of this task has
+   * been executed. The absolute time is the multiplication of "clockTick0"
+   * and "Timing.stepSize0". Size of "clockTick0" ensures timer will not
+   * overflow during the application lifespan selected.
+   */
+  rtM->Timing.t[0] =
+    ((time_T)(++rtM->Timing.clockTick0)) * rtM->Timing.stepSize0;
+
+  {
+    /* Update absolute timer for sample time: [0.2s, 0.0s] */
+    /* The "clockTick1" counts the number of times the code of this task has
+     * been executed. The resolution of this integer timer is 0.2, which is the step size
+     * of the task. Size of "clockTick1" ensures timer will not overflow during the
+     * application lifespan selected.
+     */
+    rtM->Timing.clockTick1++;
+  }
+}
+
+/* Model initialize function */
+void irrigation_initialize(void)
+{
+  /* Registration code */
+  {
+    /* Setup solver object */
+    rtsiSetSimTimeStepPtr(&rtM->solverInfo, &rtM->Timing.simTimeStep);
+    rtsiSetTPtr(&rtM->solverInfo, &rtmGetTPtr(rtM));
+    rtsiSetStepSizePtr(&rtM->solverInfo, &rtM->Timing.stepSize0);
+    rtsiSetErrorStatusPtr(&rtM->solverInfo, (&rtmGetErrorStatus(rtM)));
+    rtsiSetRTModelPtr(&rtM->solverInfo, rtM);
+  }
+
+  rtsiSetSimTimeStep(&rtM->solverInfo, MAJOR_TIME_STEP);
+  rtsiSetSolverName(&rtM->solverInfo,"FixedStepDiscrete");
+  rtmSetTPtr(rtM, &rtM->Timing.tArray[0]);
+  rtM->Timing.stepSize0 = 0.2;
+}
+
+/*
+ * File trailer for generated code.
+ *
+ * [EOF]
+ */
